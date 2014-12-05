63748b1462b7f3b2ff4a1c6dc03fb677b5c861ed
Vadim Lopatin <coolreader.org@gmail.com>
2014-12-05 15:49:40 +0300

    initial project

---
+++ src/app.d
@@ -0,0 +1,44 @@
+module app;
+
+import dlangui.all;
+import std.stdio;
+import std.conv;
+
+
+mixin APP_ENTRY_POINT;
+
+/// entry point for dlangui based application
+extern (C) int UIAppMain(string[] args) {
+    // resource directory search paths
+    string[] resourceDirs = [
+        appendPath(exePath, "../../../res/"),   // for Visual D and DUB builds
+        appendPath(exePath, "../../../res/mdpi/"),   // for Visual D and DUB builds
+        appendPath(exePath, "../../../../res/"),// for Mono-D builds
+        appendPath(exePath, "../../../../res/mdpi/"),// for Mono-D builds
+		appendPath(exePath, "res/"), // when res dir is located at the same directory as executable
+		appendPath(exePath, "../res/"), // when res dir is located at project directory
+		appendPath(exePath, "../../res/"), // when res dir is located at the same directory as executable
+		appendPath(exePath, "res/mdpi/"), // when res dir is located at the same directory as executable
+		appendPath(exePath, "../res/mdpi/"), // when res dir is located at project directory
+		appendPath(exePath, "../../res/mdpi/") // when res dir is located at the same directory as executable
+	];
+
+    // setup resource directories - will use only existing directories
+	Platform.instance.resourceDirs = resourceDirs;
+    // select translation file - for english language
+	Platform.instance.uiLanguage = "en";
+	// load theme from file "theme_default.xml"
+	Platform.instance.uiTheme = "theme_default";
+
+    // create window
+    Window window = Platform.instance.createWindow("My Window", null);
+	
+    // create some widget to show in window
+    window.mainWidget = (new Button()).text("Hello world"d).margins(Rect(20,20,20,20));
+
+    // show window
+    window.show();
+
+    // run message loop
+    return Platform.instance.enterMessageLoop();
+}
