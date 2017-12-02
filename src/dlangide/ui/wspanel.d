2cd4bc14160c9a8df327a599f7e057dedbd6f024
Zevenberge <nanayamae@gmail.com>
2017-12-02 22:07:46 +0100

    Commented the folder creation at project root level
    The files are project root are not shown (i.e. README files). Therefore, if you make a folder, it doesn't appear in the UI. The functionality is tested and works, but the scanning of root-level files should be implemented first (more importantly, a descision should be taken which files to show).

--- src/dlangide/ui/wspanel.d
+++ src/dlangide/ui/wspanel.d
@@ -103,7 +103,7 @@ class WorkspacePanel : DockWindow {
         _projectPopupMenu.add(ACTION_PROJECT_SET_STARTUP,
                               ACTION_PROJECT_FOLDER_REFRESH,
                               ACTION_FILE_NEW_SOURCE_FILE,
-                              ACTION_FILE_NEW_DIRECTORY,
+                              //ACTION_FILE_NEW_DIRECTORY,
                               //ACTION_PROJECT_FOLDER_OPEN_ITEM,
                               ACTION_PROJECT_BUILD,
                               ACTION_PROJECT_REBUILD,
